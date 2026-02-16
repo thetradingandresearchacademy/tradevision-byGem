c3309640d91f8d907f22e68901eca7b9b14c5f9e
thetradingandresearchacademy <thetradingandresearchacademy@gmail.com>
2026-02-16 18:29:16 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.SWINGLAB_CONFIG = {
+  SUPABASE_URL: "https://tfpscbilfwekzvvktinm.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmcHNjYmlsZndla3p2dmt0aW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4OTQyNDQsImV4cCI6MjA4NjQ3MDI0NH0.EbCB3tMmXJVqIk3z1xTE3T-rBIqGMbI4cEY5-WmItlk"
+};
